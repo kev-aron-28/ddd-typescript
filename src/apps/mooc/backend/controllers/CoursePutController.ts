57e0abbaf68886b77b5e261ca26cf523a191c9c2
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2024-06-01 19:36:27 -0600

    ddd

--- src/apps/mooc/backend/controllers/CoursePutController.ts
+++ src/apps/mooc/backend/controllers/CoursePutController.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
+import { CourseCreator } from '../../../../Contexts/Mooc/Courses/application/CourseCreator';
 import { Controller } from './Controller';
 
 export class CoursePutController implements Controller {
-	run(req: Request, res: Response): void {
+	constructor(private courseCreator: CourseCreator) {}
+
+	async run(req: Request, res: Response): Promise<void> {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const { id, name, duration } = req.body;
+
+		await this.courseCreator.run(id as string, name as string, duration as string);
+
 		res.status(httpStatus.CREATED).send();
 	}
 }
- 
